a69462bf6a347f6696de2ec6538516a687e17d0f
Junwoo Cheon <jwcheonx@gmail.com>
2024-07-27 23:22:38 +0900

    Constrain article title and description not to be null
    Ran `bin/rails db:migrate:reset`

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,8 @@
 
 ActiveRecord::Schema[7.1].define(version: 2024_07_27_053826) do
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
+    t.string "title", null: false
+    t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
