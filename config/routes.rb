7642f9ccd9a7e3de2ea8ee28882b8d7c1799dc50
Junwoo Cheon <jwcheonx@gmail.com>
2024-07-30 16:07:26 +0900

    Define `articles#new` and `#create` actions

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
 
-  resources :articles, only: %i[index show]
+  resources :articles, only: %i[index show new create]
 end
