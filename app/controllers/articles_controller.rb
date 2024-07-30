2377747260397a7b2546e9968c20738df61c749f
Junwoo Cheon <jwcheonx@gmail.com>
2024-07-30 18:43:54 +0900

    Define `articles#edit` and `#update` actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,12 +11,25 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
       redirect_to @article, notice: 'Article created'
     else
       render :new, status: :unprocessable_content
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article, notice: 'Article updated'
+    else
+      render :edit, status: :unprocessable_content
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
 
-  resources :articles, only: %i[index show new create]
+  resources :articles, except: :destroy
 end
