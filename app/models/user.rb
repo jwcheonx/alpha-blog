cb05c3e14ba7d145a1cb6481c61487f187ef935f
Junwoo Cheon <jwcheonx@gmail.com>
2024-08-05 17:35:06 +0900

    Convert email to downcase before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,16 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false },
             length: { maximum: 105 },
             format: URI::MailTo::EMAIL_REGEXP
+
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    # `email'         -- Calls `User#email'.
+    # `self.email'    -- Calls `User#email'.
+    # `email ='       -- Initializes a local variable.
+    # `self.email = ' -- Calls `User#email='.
+    self.email = email.downcase
+  end
 end
