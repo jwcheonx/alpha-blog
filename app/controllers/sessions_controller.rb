a608f265b0156ae0e70f6754e893cf5228409764
Junwoo Cheon <jwcheonx@gmail.com>
2024-08-13 17:41:21 +0900

    Define `sessions#destroy` action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:credentials][:email].downcase)
     if user&.authenticate(params[:credentials][:password])
+      session[:user_id] = user.id
       redirect_to user, notice: 'Logged in'
     else
       flash.now.alert = 'Invalid email or password'
       render :new, status: :unauthorized
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'Logged out'
   end
 end
