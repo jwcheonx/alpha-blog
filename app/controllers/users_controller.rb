c06f388382a333c58531815b3f244eed79c83ba7
Junwoo Cheon <jwcheonx@gmail.com>
2024-08-13 18:03:59 +0900

    Store user ID in session right after signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user,
                   notice:
                     "Welcome to the Alpha Blog, #{@user.username}. You've successfully signed up."
