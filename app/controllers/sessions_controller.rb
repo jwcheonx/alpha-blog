9854f1861a8d80c3aade1d39207aff786c42ab36
Junwoo Cheon <jwcheonx@gmail.com>
2024-08-13 16:08:47 +0900

    Define `sessions#create` action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,13 @@
 class SessionsController < ApplicationController
   def new; end
+
+  def create
+    user = User.find_by(email: params[:credentials][:email].downcase)
+    if user&.authenticate(params[:credentials][:password])
+      redirect_to user, notice: 'Logged in'
+    else
+      flash.now.alert = 'Invalid email or password'
+      render :new, status: :unauthorized
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   resources :users, only: %i[index show edit create update]
 
   get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
 end
