6ec4711adb15eba767ccf58b8c60e00c52f4969b
Junwoo Cheon <jwcheonx@gmail.com>
2024-08-18 16:36:30 +0900

    Test validation on length of category name

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -18,4 +18,14 @@ class CategoryTest < ActiveSupport::TestCase
     @category.save
     assert_not @category.dup.valid?
   end
+
+  test 'name should not be too long' do
+    @category.name = 'a' * 26
+    assert_not @category.valid?
+  end
+
+  test 'name should not be too short' do
+    @category.name = 'aa'
+    assert_not @category.valid?
+  end
 end
