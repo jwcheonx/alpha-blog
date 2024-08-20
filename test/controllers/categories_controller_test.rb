1f8199a485664429339ddab84ae783f2e293532b
Junwoo Cheon <jwcheonx@gmail.com>
2024-08-20 21:07:37 +0900

    Test creating no categories if not logged-in or unauthorized

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -34,4 +34,22 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to category_path(Category.last)
   end
+
+  test 'should not create category if not logged in' do
+    assert_no_difference(-> { Category.count }) do
+      post categories_path, params: { category: { name: 'Travel' } }
+    end
+
+    assert_redirected_to login_path
+  end
+
+  test 'should not create category if not admin' do
+    log_in_as User.create!(username: 'marymoe', email: 'mmoe@example.org', password: 'password')
+
+    assert_no_difference(-> { Category.count }) do
+      post categories_path, params: { category: { name: 'Travel' } }
+    end
+
+    assert_redirected_to categories_path
+  end
 end
